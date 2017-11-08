be029d22ee5cd4adabd55224628b3f43662726f9
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-11-08 14:42:01 +1300

    Using object propt for JSX rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,37 +4,56 @@ console.log('App js is the running');
 
 // JSX - JavaScript XML
 
-// var template = (
-//   <div>
-//     <h1>This is a test of JSX</h1>
-//     <p>This is some text</p>
-//     <ol>
-//       <li>item one</li>
-//       <li>item two</li>
-//     </ol>
-//   </div>
-// )
-
-var templateTwo = React.createElement(
+var app = {
+  title: 'Ask Jim',
+  subtitle: 'A friendly decision maker'
+};
+var template = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    'Ali Kahwaji'
+    app.title.toUpperCase()
   ),
   React.createElement(
     'p',
     null,
-    'Age: 37'
+    app.subtitle
   ),
   React.createElement(
-    'p',
+    'ol',
     null,
-    'Location: Auckland'
+    React.createElement(
+      'li',
+      null,
+      'item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'item two'
+    )
   )
 );
 
+// var user = {
+//   name: 'Mike',
+//   age: 37,
+//   location: 'NZ'
+// }
+// var userName = 'ali'
+// var userAge = 30
+// var userLocation = 'Auckland'
+
+// var templateTwo = (
+//   <div>
+//     <h1>{user.name.toUpperCase()}</h1>
+//     <p>{`Age: ${user.age}.`}</p>
+//     <p>{`Location: ${user.location}.`}</p>
+//   </div>
+// )
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -2,25 +2,38 @@ console.log('App js is the running')
 
 // JSX - JavaScript XML
 
-// var template = (
-//   <div>
-//     <h1>This is a test of JSX</h1>
-//     <p>This is some text</p>
-//     <ol>
-//       <li>item one</li>
-//       <li>item two</li>
-//     </ol>
-//   </div>
-// )
-
-var templateTwo = (
+var app = {
+  title: 'Ask Jim',
+  subtitle: 'A friendly decision maker'
+}
+var template = (
   <div>
-    <h1>Ali Kahwaji</h1>
-    <p>Age: 37</p>
-    <p>Location: Auckland</p>
+    <h1>{app.title.toUpperCase()}</h1>
+    <p>{app.subtitle}</p>
+    <ol>
+      <li>item one</li>
+      <li>item two</li>
+    </ol>
   </div>
 )
 
+// var user = {
+//   name: 'Mike',
+//   age: 37,
+//   location: 'NZ'
+// }
+// var userName = 'ali'
+// var userAge = 30
+// var userLocation = 'Auckland'
+
+// var templateTwo = (
+//   <div>
+//     <h1>{user.name.toUpperCase()}</h1>
+//     <p>{`Age: ${user.age}.`}</p>
+//     <p>{`Location: ${user.location}.`}</p>
+//   </div>
+// )
+
 var appRoot = document.getElementById('app')
 
-ReactDOM.render(templateTwo, appRoot)
+ReactDOM.render(template, appRoot)
