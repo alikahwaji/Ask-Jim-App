6cbe5d4afe8e29df35f48023036c2db16f284251
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-11-11 13:54:35 +1300

    Creating a sub class and over-writing methods

--- src/JSX/es6-classes.js
+++ src/JSX/es6-classes.js
@@ -4,15 +4,50 @@ class Person {
     this.age = age
   }
   getGreeting () {
-    return `Hi. I am ${this.name}!`
+    return `Hi. I am ${this.name}.`
   }
   getDescription () {
-    return ` This is ${this.name} and his is ${this.age}.`
+    return `Hi. I am ${this.name},I'm ${this.age} years old.`
   }
 }
 
-const myName = new Person('Ali Kahwaji', 37)
-console.log(myName.getDescription())
+// const myInfo = new Person('Ali', 37)
+// console.log(myInfo.getDescription())
 
-const myLastName = new Person()
-console.log(myLastName.getDescription())
+class Student extends Person {
+  constructor (name, age, major) {
+    super(name, age)
+    this.major = major
+  }
+  hasMajor () {
+    return this.major
+  }
+  getDescription () {
+    let description = super.getDescription()
+    if (this.hasMajor()) {
+      description += ` I have ${this.major} major.`
+    }
+    return description
+  }
+}
+
+const myFullInfo = new Student('Ali', 37, 'Information Technology')
+console.log(myFullInfo.getDescription())
+
+class Traveler extends Person {
+  constructor (name, age, homeLocation) {
+    super(name, age)
+    this.homeLocation = homeLocation
+  }
+
+  getGreeting () {
+    let greeting = super.getGreeting()
+    if (this.homeLocation) {
+      greeting += `I'm visiting from ${this.homeLocation}.`
+    }
+    return greeting
+  }
+}
+
+const myHomeLoction = new Traveler('Ali', 37, 'Auckland')
+console.log(myHomeLoction.getGreeting())
