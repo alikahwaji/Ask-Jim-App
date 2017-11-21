8aae495c10a9bbb17e9909a6b3e7810ee43f5775
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-11-22 11:50:46 +1300

    Installing and config webpack with sass and css loader

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MainApp from './components/MainApp'
+import './styles/styles.scss'
 
 ReactDOM.render(<MainApp />, document.getElementById('app'))
