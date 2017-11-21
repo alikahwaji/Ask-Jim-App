c61b8cadbcf5c654722957b88d72834cb5b418cf
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-11-21 16:55:49 +1300

    Adding validator package

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// import './utilis.js'
+import validator from 'validator'
 import isSenior, {square, add} from './utilis.js'
 
 console.log('app.js is running!')
@@ -9,3 +9,5 @@ console.log(isSenior(65))
 // console.log(subtract(100, 81))
 // console.log(isAdult(19))
 // console.log(canDrink(18))
+
+console.log(validator.isEmail('test@gmail.com'))
