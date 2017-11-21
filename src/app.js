0fbd42175b9ce1cdc8b364f582af59607fa2ec2a
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-11-21 17:11:15 +1300

    Installing react and react-dom

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-import validator from 'validator'
-import isSenior, {square, add} from './utilis.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-console.log('app.js is running!')
-
-console.log(square(4))
-console.log(add(100, 23))
-console.log(isSenior(65))
-// console.log(subtract(100, 81))
-// console.log(isAdult(19))
-// console.log(canDrink(18))
-
-console.log(validator.isEmail('test@gmail.com'))
+const template = React.createElement('p', {}, 'testing 123')
+ReactDOM.render(template, document.getElementById('app'))
