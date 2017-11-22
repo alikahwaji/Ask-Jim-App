8cc6366dc36dc6ac776e2f895efcbd7cf2b759e2
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-11-23 09:32:13 +1300

    Styling React modal box and break-word

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleClearSelectedOption}
     contentLabel='Selected options'
+    closeTimeoutMS={100}
+    className='modal'
   >
-    <h3>Selected option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>okay</button>
+    <h3 className='modal__title'>Selected option</h3>
+    {props.selectedOption && <p className='modal__body'>{props.selectedOption}</p>}
+    <button className='button' onClick={props.handleClearSelectedOption}>okay</button>
   </Modal>
 
 )
