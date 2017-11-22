533ff4fe5f46a2a7ce475b9a4e04437a3272faa7
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-11-22 19:13:22 +1300

    Installing normalize.css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MainApp from './components/MainApp'
+import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 ReactDOM.render(<MainApp />, document.getElementById('app'))
